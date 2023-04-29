20085c3ee498808080ce3c9daded74d1bbffcc90
scottyang <67733409+scott-yj-yang@users.noreply.github.com>
2023-04-29 01:12:27 -0700

    clean notebook, add nbgrader support

--- GradescopeLatePenalty/nbgrader.py
+++ GradescopeLatePenalty/nbgrader.py
@@ -10,6 +10,7 @@ import numpy as np
 import pandas as pd
 import json
 from datetime import datetime
+import yaml
 
 # %% ../nbs/api/01_nbgrader_process_grade.ipynb 4
 class bcolors:
@@ -26,11 +27,12 @@ class bcolors:
 # %% ../nbs/api/01_nbgrader_process_grade.ipynb 5
 class nbgrader_grade:
     def __init__(self,
-                 credentials_fp = "", # credential file path. [Template of the credentials.json](https://github.com/FleischerResearchLab/CanvasGroupy/blob/main/nbs/credentials.json)
+                 credentials_fp="", # credential file path. [Template of the credentials.json](https://github.com/FleischerResearchLab/CanvasGroupy/blob/main/nbs/credentials.json)
+                 late_exception_fp="", # late exception yaml file path. [Template of the late_exception.yaml](https://github.com/scott-yj-yang/GradescopeLatePenalty/tree/main/nbs/api/late_exception.yaml)
                  API_URL="https://canvas.ucsd.edu", # the domain name of canvas
                  course_id="", # Course ID, can be found in the course url
                  assignment_id=-1, # assignment id, can be found in the canvas assignment url
-                 gradescope_fp="", # gradescope csv file path 
+                 grades_fp="", # nbgrader csv grades exports file path 
                  verbosity=0 # Controls the verbosity: 0 = Silent, 1 = print all messages
                 ):
         "Initialize Canvas Group within a Group Set and its appropriate memberships"
@@ -43,7 +45,10 @@ class nbgrader_grade:
         self.API_KEY = None
         self.verbosity = verbosity
         self.assignment = None
-        self.gradescope = None
+        self.grades = None
+        self.late_exception = dict()
+        self.grades_by_assignment = dict()
+        self.late_days_by_assignment = dict()
         
         # initialize by the input parameter
         if credentials_fp != "":
@@ -52,8 +57,10 @@ class nbgrader_grade:
             self.set_course(course_id)
         if assignment_id != -1:
             self.link_assignment(assignment_id)
-        if gradescope_fp != "":
-            self.load_gradescope_csv(gradescope_fp)
+        if late_exception_fp != "":
+            self.load_late_exception(late_exception_fp)
+        if grades_fp != "":
+            self.load_grades_csv(grades_fp)
         
     def auth_canvas(self,
                     credentials_fp: str # the Authenticator key generated from canvas
@@ -101,47 +108,82 @@ class nbgrader_grade:
         self.assignment = assignment
         return assignment
                     
-    def load_gradescope_csv(self,
-                            csv_pf:str # csv file path 
+    def load_grades_csv(self,
+                        csv_pf:str # csv file path 
+                       ):
+        "Load nbgrader exported csv file"
+        self.grades = pd.read_csv(csv_pf)
+        self._parse_assignments()
+    
+    def load_late_exception(self,
+                            yaml_fp:str # yaml file path stores exception student cases
                            ):
-        "Load gradescope exported csv file"
-        self.gradescope = pd.read_csv(csv_pf)
-        self.gradescope['Email'] = self.gradescope["Email"].str.split("@").str[0]
-        self.gradescope = self.gradescope.set_index("Email")
-        self.gradescope = self.gradescope.fillna(0)
+        "Load Late Exception File"
+        with open(yaml_fp, "r") as f:
+            self.late_exception = yaml.safe_load(f)
         
-    def calculate_late_hour(self,
-                            target_assignment:str, # target assignment name. Must in the column of gradescope csv 
-                           ) -> pd.Series: # late hours of the target assignment
-        "Calculate the late hours of each submission of the target assignment"
-        late_col_name = f"{target_assignment} - Lateness (H:M:S)"
-        late_col = self.gradescope[late_col_name]
-        # calculate how many slip day (hours) used for this assignment.
-        late_hours = (
-            late_col.str.split(":").str[0].astype(int) + 
-            np.ceil(late_col.str.split(":").str[1].astype(int)/60)
-        )
-        return late_hours
+    def _parse_assignments(self):
+        "Parse all assignment by assignment name. And calculate late days used."
+        if len(self.grades) == 0:
+            raise ValueError("grades has not been loaded. Please loaded via self.load_grades_csv")
+        assignments = self.grades["assignment"].unique()
+        # I am just lazy :-)
+        df = self.grades
+        for assignment in assignments:
+            A = df[df["assignment"] == assignment]
+            # filter those who submitted
+            A = A[~A["timestamp"].isna()].copy()
+            # remove the redundant /
+            A["student_id"] = A["student_id"].str.split("/").str[0]
+            A = A.set_index("student_id")
+            slip_day_used = self._calculate_late_days(A)
+            A["slip_day_used"] = slip_day_used
+            # store the parsed result
+            self.grades_by_assignment[assignment] = A
+            self.late_days_by_assignment[assignment] = A["slip_day_used"]
+    
+    def _calculate_late_days(self,
+                             df:pd.DataFrame # dataframe of a specific assignment
+                            )-> pd.Series: # late days
+        # parse the timestamp
+        duedate_format = "%Y-%m-%d %H:%M:%S"
+        timestamp_format = "%Y-%m-%d %H:%M:%S.%f"
+        df["duedate"] = df["duedate"].apply(lambda x: datetime.strptime(x, duedate_format))
+        df["timestamp"] = df["timestamp"].apply(lambda x: datetime.strptime(x, timestamp_format))
+        late_time_delta = (df["timestamp"] - df["duedate"])
+        # calculate late days, use ReLU
+        slip_day_used = late_time_delta.apply(lambda x: np.max([np.ceil(x.total_seconds()/60/60/24), 0]))
+        return slip_day_used
+    
+    def get_late_days(self,
+                      target_assignment:str, # target assignment name. Must in the column of nbgrader assignment csv 
+                      student_id:str # student id
+                     ) -> int: # late days of the target assignment
+        "Calculate the late day of students submission of the target assignment"
+        try:
+            late_day = self.late_days_by_assignment[target_assignment][student_id]
+        except KeyError:
+            if self.verbosity != 1:
+                print(f"Student {bcolors.WARNING+student_id+bcolors.ENDC} did "
+                      f"not submit {bcolors.WARNING+target_assignment+bcolors.ENDC}")
+            late_day = 0
+        return late_day
         
     def calculate_credit_balance(self,
-                                 passed_assignments:[str], # list of passed assignment. Must in the column of gradescope csv
-                                 total_credit = 120 # total number of allowed late hours
-                                ) -> dict: # {email: credit balance} late credit balance of each students
-        "Calculate the balance of late hours from the gradescope file"
-        self.gradescope["late balance"] = total_credit
+                                 passed_assignments:[str], # list of passed assignments name. Must in the column of `assignment`.
+                                 student_id:str, # target student
+                                 default_credit = 5 # default total number of allowed late days
+                                ) -> int: # late credit balance of the target student
+        "Calculate the balance of late hours from the nbgrader file"
+        # if student is in the late exception, use the new number
+        if student_id in self.late_exception:
+            default_credit = self.late_exception[student_id]["allowed_late_days"]
         for passed in passed_assignments:
-            late_hours = self.calculate_late_hour(passed)
-            self.gradescope["late balance"] = self.gradescope["late balance"] - late_hours
-        return self.gradescope["late balance"]
-    
-    def calculate_total_score(self,
-                              components:[str], # components of a single assignment. Must in the column of gradescope csv
-                             ) -> pd.Series:
-        "Calculate the total score of an assignment"
-        self.gradescope["target_total"] = 0
-        for component in components:
-            self.gradescope["target_total"] += self.gradescope[component]
-        return self.gradescope["target_total"]
+            late_days = self.get_late_days(passed, student_id)
+            if late_days <= default_credit:
+                # means this passed assignment did not get penalty
+                default_credit -= self.get_late_days(passed, student_id)
+        return default_credit
     
     def _post_grade(self,
                    student_id: int, # canvas student id of student. found in self.email_to_canvas_id
@@ -150,56 +192,66 @@ class nbgrader_grade:
                   ) -> canvasapi.submission.Submission: # created submission
         "Post grade and comment to canvas to the target assignment"
         submission = self.assignment.get_submission(student_id)
-        edited = submission.edit(
-            submission={
-                'posted_grade': grade
-            }, comment={
-                'text_comment': text_comment
-            }
-        )
+        if grade is not None:
+            edited = submission.edit(
+                submission={
+                    'posted_grade': grade
+                }, comment={
+                    'text_comment': text_comment
+                }
+            )
+        else:
+            edited = submission.edit(
+                comment={
+                    'text_comment': text_comment
+                }
+            )
         if self.verbosity != 0:
             print(f"Grade for {bcolors.OKGREEN+email+bcolors.ENDC} Posted!")
         return edited
     
     def post_to_canvas(self,
-                       target_assignment:str, # target assignment name to grab the late time. Must in the column of gradescope csv 
-                       passed_assignments:[str], # list of passed assignment. Must in the column of gradescope csv
-                       components:[str], # components of a single assignment. Must in the column of gradescope csv
+                       target_assignment:str, # target assignment name to grab the late time. Must in the column of nbgrader assignment csv 
+                       passed_assignments:[str], # list of passed assignment. Must in the column of nbgrader assignment csv
                        post=True, # for testing purposes. Can hault the post operation
                       ):
         "Post grade to canvas with late penalty."
-        if self.gradescope is None:
-            raise ValueError("Gradescope CSV has not been loaded. Please set it via process_grade.load_gradescope_csv")
-        credit_balance = self.calculate_credit_balance(passed_assignments)
-        late_hours = self.calculate_late_hour(target_assignment)
-        if len(components) > 1:
-            total_score = self.calculate_total_score(components)
-        else:
-            total_score = self.gradescope[target_assignment]
-        # Post Grade
-        for email, _ in self.gradescope.iterrows():
-            remaining = credit_balance[email]
-            score, slip_hour = round(total_score[email], 4), late_hours[email]
+        if self.grades is None:
+            raise ValueError("Nbgrader CSV has not been loaded. Please set it via self.load_grades_csv")
+        for student_id, row in self.grades_by_assignment[target_assignment].iterrows():
+            penalty = False
+            # fetch useful information
+            balance = self.calculate_credit_balance(passed_assignments, student_id)
+            late_days = self.get_late_days(target_assignment, student_id)
+            score = row["raw_score"]
+            # build message for each student
             message = f"{target_assignment}: \n"
-            if slip_hour > 0:
+            if late_days > 0:
                 # means late submission. Check remaining slip day
-                message += f"Late Submission: {int(slip_hour)} Hours Late\n"
-                if remaining - slip_hour < 0:
+                message += f"Late Submission: {int(late_days)} Days Late\n"
+                if balance - late_days < 0:
                     message += "Insufficient Slip Credit. 25% late penalty applied\n"
                     score = round(score * 0.75, 4)
+                    penalty = True
                 else:
                     message += "Slip Credit Used. No late penalty applied\n"
             else:
                 message += "Submitted intime\n"
-            balance_after = remaining - slip_hour
-            message += f"Remaining Slip Credit: {int(balance_after)} Hours"
+            if not penalty:
+                # if student did not get penalized and use the slip day
+                balance_after = balance - late_days
+            else:
+                # if the student did get penalized and did not use the slip day
+                balance_after = balance
+            message += f"Remaining Slip Credit: {int(balance_after)} Days"
             try:
                 if post:
-                    student_id = grade.email_to_canvas_id[email.split("@")[0]]
-                    grade._post_grade(grade=score, student_id=student_id, text_comment=message)
+                    canvas_student_id = grade.email_to_canvas_id[student_id]
+                    grade._post_grade(grade=score, student_id=canvas_student_id, text_comment=message)
                 else:
                     print(f"{bcolors.WARNING}Post Disabled{bcolors.ENDC}\n"
-                          f"The message is: \n{bcolors.OKGREEN+message+bcolors.ENDC}"
+                          f"The message for {bcolors.OKCYAN+student_id+bcolors.ENDC} "
+                          f"is: \n{bcolors.OKGREEN+message+bcolors.ENDC}\n"
                          )
             except Exception as e:
                 print(f"Studnet: {bcolors.WARNING+email+bcolors.ENDC} Not found on canvas. \n"
